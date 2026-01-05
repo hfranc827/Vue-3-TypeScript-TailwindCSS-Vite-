41b4569983a811b044200db83614ddcb992b35bc
hfran827 <sunefranco3@gmail.com>
2026-01-04 21:05:23 -0500

    primer cambio

---
+++ web_design1/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-black">
+    <h1 class="text-6xl font-bold text-pink-500">
+      GAMMA POLOS
+    </h1>
+  </div>
+</template>
